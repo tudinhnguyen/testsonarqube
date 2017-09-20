25e14ada96eaef5bb7e2537a2aa9256fabb1116a
tudinhnguyen <32124199+tudinhnguyen@users.noreply.github.com>
2017-09-20 14:46:53 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Teechip.View;
+using Teechip.Controller;
+
+namespace Teechip
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+		static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
+
+            Application.Run(new frmURLShortener());
+        }
+
+        static void Application_ApplicationExit(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
